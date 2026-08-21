e39f43a300cb4daf4380e13caef61fe49b447919
Ankit Nishad <ankitn575@gmail.com>
2026-08-21 11:42:25 +0530

    Update project

--- Hashing/Practice.java
+++ Hashing/Practice.java
@@ -1,5 +1,31 @@
 
 import java.util.*;
 public class Practice {
+
+    public static void main(String[] args) {
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        hm.put("India", 100);
+        hm.put("China", 200);
+        hm.put("Indonesia", 10);
+        hm.put("Brazil", 10);
+
+       
+
+        Set<String> keys = hm.keySet();
+
+         System.out.println(keys);
+
+         for (String k : keys) {
+            System.out.println("Key="+k+", value="+hm.get(k));
+            
+         }
+
+
+
+
+
+    }
+
     
 }
